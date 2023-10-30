e97a53a5ce5b52e8650a19e411ff7f7f6930be6e
Yannick Sereckissy-Namboy <yannick.sereckissy@gmail.com>
2023-10-30 13:48:34 +0100

    continue implementation of the dashboard

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,24 @@
 import Link from "next/link";
+import {lusitana} from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import {CreateInvoice} from "@/app/ui/invoices/buttons";
 
-const Page = () => {
+const Page = async () => {
     return (
-        <div>
-            <h1>Invoices Page</h1>
-            <Link href={`/dashboard`} className={`block`}>Back to Dashboard</Link>
+        <div className={`w-full`}>
+            <div className={`flex w-full items-center justify-between`}>
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className={`mt-4 flex items-center justify-between gap-2 md:mt-8`}>
+                <Search placeholder={`Search invoices...`} />
+                <CreateInvoice />
+            </div>
+            {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+            <div className="mt-5 flex w-full justify-center">
+                {/* <Pagination totalPages={totalPages} /> */}
+            </div>
         </div>
     );
 };
